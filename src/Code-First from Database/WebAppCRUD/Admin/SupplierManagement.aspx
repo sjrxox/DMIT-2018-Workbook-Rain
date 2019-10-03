d587666ecdfbe6bbd8dc2dcbe19bca60b0746e7c
sjrxox <46492908+sjrxox@users.noreply.github.com>
2019-10-03 11:14:38 -0600

    problem with supplier manager

---
+++ src/Code-First from Database/WebAppCRUD/Admin/SupplierManagement.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SupplierManagement.aspx.cs" Inherits="WebAppCRUD.Admin.SupplierManagement" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Supplier Management System</h1>
+    <h2>Supplier Summary</h2>
+
+    <asp:Repeater ID="SupplierSummaryRepeater" runat="server" DataSourceID="SupplierSummaryDataSource"></asp:Repeater>
+    <asp:ObjectDataSource ID="SupplierSummaryDataSource" runat="server"></asp:ObjectDataSource>
+</asp:Content>
---
+++ src/Code-First from Database/WestWindSystem/BLL/SupplierManager.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using WestWindSystem.DAL;
+
+namespace WestWindSystem.BLL
+{
+    [DataObject]
+    public class SupplierManager
+    {
+        public void ListSupplierSummary()
+        {
+            using (var context = new WestWindContext()
+            {
+                    var result = from vendor in Suppliers
+    select new SupplierSummary
+    {
+        CompanyName = vendor.CompanyName,
+        Contact = vendor.ContactName,
+        Phone = vendor.Phone,
+        Products = from item in vendor.Products
+                    select new SupplierProduct
+                    {
+                        Name = item.ProductName,
+                        Category = item.Category.CategoryName,
+                        Price = item.UnitPrice,
+                        PerUnitQuantity = item.QuantityPerUnit
+                    }
+
+    };
+
+        }
+    }
+}
+}
