d0613e946f3ddb20f37d8a2b5e8a8a27334b568e
sjrxox <46492908+sjrxox@users.noreply.github.com>
2019-10-22 15:59:15 -0600

    Made new console app

---
+++ src/RmedialOOP/RmedialOOP/Bunch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RmedialOOP
+{
+    public class Node<T>
+    {
+        public T Thing { get; set; }
+        public Node<T> Next { get; set; }
+    }
+
+
+    public class Bunch<T>
+    {
+        // Private Fields
+        private Node<T> _FirstItem;
+        private Node<T> _LastName;
+
+        public void Add(T item)
+        {
+            if(_FirstItem == null)
+            {
+                _FirstItem = new Node<T> { Thing = item };
+                _LastName = _FirstItem;
+            }
+        }
+    }
+}
