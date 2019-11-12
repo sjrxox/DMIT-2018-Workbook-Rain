7673ed97122e36589df151bc3569e82eb1b63446
sjrxox <46492908+sjrxox@users.noreply.github.com>
2019-11-12 15:29:07 -0700

    added POCOs/DTO/s for OrderProcessing Scenario

---
+++ src/West Wind Demo/WestWindSystem/DAL/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DAL
+{
+    public class OrderItem
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public short Qty { get; set; }
+        public string QtyPerUnit { get; set; }
+        public short Outstanding { get; set; } // TODO: Calculated as OD.Quantity - Sum(Shipped qty)
+    }
+}
