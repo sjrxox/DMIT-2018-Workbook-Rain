5baa851ae08a49065b30adde16f1600b3030ff89
sjrxox <46492908+sjrxox@users.noreply.github.com>
2019-11-12 15:37:55 -0700

    Order Processing stub out BLL methods

---
+++ src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.DataModels;
+
+namespace WestWindSystem.BLL
+{
+    public class OrderProcessingController
+    {
+        public List<OutstandingOrder> LoadOrders(int supplierId)
+        {
+            throw new NotImplementedException();
+            // TODO: Implement this method with the following
+            /*
+             * Validation:
+                    Make sure the supplier ID exixts, otherwise throw an exception
+                    [Advanced] Make sure the logged-in user works for the identified supplier.
+                Query for outstanding orders, getting data from the following tables:
+                    TODO: List table names
+             */
+        }
+
+        public List<ShipperSelection> ListShipper()
+        {
+            throw new NotImplementedException();
+            // TODO: Get all the shippers from the Db
+        }
+
+        public void ShipOrder(int orderId, ShippingDirections shipping, List<ShippedItem> items)
+        {
+            throw new NotImplementedException();
+            // TODO: Validation Steps
+            /*Validation:
+                OrderId must be valid
+                ShippingDirections is required (cannot be null)
+                List<ShippedItem> cannot be empty/null
+                The product must be on the order AND items that this supplier provides
+                Quantities must be greater than zero and less than or equal to the quantity outstanding
+                Shipper must exists
+                Freight charge must be either null (no charge) or greater than zero (> $0.00)
+             */
+            // TODO: Process the order shipment
+            /*
+             * Processing (tables/data that must be updated/inserted/deleted/whatever)
+                Create new Shipment
+                Add all manifest items
+                Check if order is complete; if so, update Order.Shipped
+             */
+        }
+    }
+}
